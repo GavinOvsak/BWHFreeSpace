f837c3311773f3d35abb713810e53c8db1a7907f
Gavin Ovsak <ovsak.gavin@gmail.com>
2020-10-02 12:36:31 -0400

    u

--- data/spaceList.js
+++ data/spaceList.js
@@ -1,3 +1,3 @@
 var spaceList = 
-{"s22":{"id":"s22","name":"BEI","description":"","link":"","spots":{"d12":{"id":"d12","name":"S1","bookings":[]},"d36":{"id":"d36","name":"S3","bookings":[{"start":1601649460133,"len":30,"name":"GGO"}]}}},"s19":{"id":"s19","name":"Test","description":"","link":"","spots":{}}}; 
+{"s22":{"id":"s22","name":"BEI","description":"","link":"","spots":{"d12":{"id":"d12","name":"S1","bookings":[{"start":1601650085905,"len":30,"name":"GGO"}]},"d36":{"id":"d36","name":"S3","bookings":[{"start":1601649460133,"len":30,"name":"GGO"}]}}}}; 
 if (typeof window === 'undefined') { module.exports = spaceList;}
